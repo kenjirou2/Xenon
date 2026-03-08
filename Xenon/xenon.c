54f15f5ce91b77366ba5e9f08cc55c556866a24b
koram kram <zorojoru31@gmail.com>
2026-03-08 16:04:14 +0400

    Added socketing code (still under development)

---
+++ Xenon/xenon.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include "Dependencies/driver/windows.h"
+#include "Dependencies/driver/socket.h"
+#include "Dependencies/driver/memory.h"
+
+#define KILOBYTE 1024
+
+int Socinfo();
+char *Socket();
+int RnSD(SOCKET socket);
+
+
+int main()
+{
+
+	uint16_t PORT = 0;
+	uint32_t ipaddr = 0;
+
+	while (PORT == 0 && ipaddr == 0)
+	{
+		printf("PORT> ");
+		scanf("%d", &PORT);
+		printf("IP> ");
+		scanf("%s", &ipaddr);
+	}
+
+	Socinfo();
+
+	return 0;
+
+}
+
+int Socinfo()
+{
+
+	struct addrinfo *result = NULL, *ptr = NULL, socinfo;
+	
+	ZeroMemory(&socinfo, sizeof(socinfo));
+	socinfo.ai_family = AF_INET;
+	socinfo.ai_socktype = SOCK_STREAM;
+	socinfo.ai_protocol = IPPROTO_TCP;
+	socinfo.ai_flags = AI_PASSIVE;
+
+	getaddri = getaddrinfo(NULL, PORT, &socinfo, &result);
+	if (getaddri != 0) {
+		printf("[-] Failed to retrive socket info");
+		return 1;
+	}
+
+	if (socket())
+	{
+		printf("\n%s",socket);
+	}
+
+}
+
+char *Socket()
+{
+
+	struct sockaddr_in server;
+
+	uint64_t Soc = INVALID_SOCKET;
+	uint64_t WSAAPI ACSoc = INVALID_SOCKET;
+	int BIND = NULL;
+	int LISTEN = NULL;
+
+
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = ipaddr;
+	server.sin_port = htons(PORT);
+
+	Soc = socket(AF_INET, SOCK_STREAM, 0);
+	if (Soc == NULL)
+	{
+		printf("[-] Failed to create socket");
+		WSACleanup();
+		return 1;
+	}
+
+	BIND = bind(Soc, (struct sockaddr_in)&server, sizeof(server));
+	if (BIND == NULL)
+	{
+		printf("[-]:::ERROR::In Func::socket:::");
+		freeaddrinfo(BIND);
+		closesocket(Soc);
+		WSACleanup();
+		return "Socket failed to bind";
+	}
+
+	LISTEN = listen(Soc, 5);
+	if (LISTEN == NULL)
+	{
+		printf("[-]:::ERROR::In Func::socket:::");
+		closesocket(Soc);
+		WSACleanup();
+		return "Socket failed to listen";
+	}
+
+	ACSoc = accept(Soc, NULL, NULL);
+	if (ACSoc == NULL)
+	{
+		printf("[-]:::ERROR::In Func::socket:::");
+		WSACleanup();
+		closesocket(Soc);
+		return "Socket failed to recive connection";
+	}
+
+	RnSD(Soc);
+
+}
+
+int RnSD(SOCKET socket)
+{
+
+	const char recvBuff[KILOBYTE];
+	const char sendBuff[KILOBYTE];
+
+	int recvresult = NULL;
+	int sendresult = NULL;
+
+	recvresult = recv(socket, recvBuff, sizeof(recvBuff), 0);
+
+	
+}
