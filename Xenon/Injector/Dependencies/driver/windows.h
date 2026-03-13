3d99cc3219f17dea78cbc60d5da8977a146aa4a9
koram kram <zorojoru31@gmail.com>
2026-03-13 22:16:39 +0400

    Updated file structure agian and fixed Depnednecies and Include folder in startup fodler + working on injector and other parts of the project

--- Xenon/Injector/Dependencies/driver/windows.h
+++ Xenon/Injector/Dependencies/driver/windows.h
@@ -1,6 +1,17 @@
-#ifndef NTkernel
-#define NTkernel
+#ifndef NTapi
+#define NTapi
 
 #include <windows.h>
+
+typedef struct{
+	
+	IMAGE_DOS_HEADER IMDOSHeader;
+	IMAGE_NT_HEADERS IMNTHeader;
+	IMAGE_OPTIONAL_HEADER IMOptionalHeader;
+	IMAGE_FILE_HEADER IMFileHeader;
+	IMAGE_SECTION_HEADER IMSectionHeader;
+
+} PE_HEADER;
+
 
 #endif
