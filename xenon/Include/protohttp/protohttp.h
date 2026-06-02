c49ca5ab413536de9a8948fa107acaf0d26f4634
koram kram <zorojoru31@gmail.com>
2026-06-02 16:12:22 +0400

    fixed sending issues with protohttp.h as well as added a counter to check amount of data sent by SSL_WRITE_EXTENDED()

--- xenon/Include/protohttp/protohttp.h
+++ xenon/Include/protohttp/protohttp.h
@@ -122,7 +122,7 @@ SSL_CTX SSLCTX() {
 int WSAIntilize()
 {
 	WSADATA wsa;
-	if (WSAStartup(WORD v=2, &wsa) != 0) {
+	if (WSAStartup(0x0032, &wsa) != 0) {
 		fprintf(stderr, "\nWSAStartup failed\n");
 		return 1;
 	}
@@ -131,7 +131,6 @@ int WSAIntilize()
 
 REQUEST Httpbuild(const char* type)
 {
-
 
 	if (strcmp(type, "GET") == 0) { return GET; }
 
@@ -142,7 +141,6 @@ REQUEST Httpbuild(const char* type)
 	if (strcmp(type, "DELETE_") == 0) { return DELETE_; }
 
 	else { return UNKNOWN; }
-
 
 }
 
@@ -161,7 +159,7 @@ void HttpbuildRequest(const char* rtype, const char* HOST, char* request, size_t
 SOCKET HttpOpenBridge(const char* HOST, const char* port, struct addrinfo** rslt)
 {
 
-	struct addrinfo* result=NULL;
+	struct addrinfo* result = NULL;
 	struct addrinfo socinfo;
 
 	SOCKET ConnectSocket = INVALID_SOCKET;
@@ -203,7 +201,11 @@ SSL* WrapSocketTLS(SSL_CTX* ctx, SOCKET sock, const char* HOST)
 {
 
 	SSL* ssl = SSL_new(ctx);
-	if (!ssl) { return NULL; }
+	if (ssl == NULL)
+	{
+		fprintf(stderr, "\n failed to create SSl object");
+		return NULL;
+	}
 
 	SSL_set_fd(ssl, (int)sock);
 	SSL_set_tlsext_host_name(ssl, HOST);
@@ -226,6 +228,7 @@ void CloseTLS(SSL* ssl, SSL_CTX* ctx, SOCKET sock)
 	if (ctx) SSL_CTX_free(ctx);
 
 	closesocket(sock);
+	return;
 
 }
 
@@ -248,15 +251,23 @@ int Httpconnect(const SOCKET soc, struct addrinfo* rslt)
 
 }
 
-const char* HttpsendSSL(SSL* ssl, const char* buffer)
+int HttpsendSSL(SSL* ssl, const char* buffer, size_t written)
 {
 
-	int sent = SSL_write(ssl, buffer, (int)strlen(buffer));
-	if (sent <= 0) { return "send failed"; }
+	if (SSL_write_ex(ssl, buffer, sizeof(buffer), written) != 1)
+	{
+		fprintf(stderr, "\nfailed to send data stream to server");
+		return 1;
+	}
+	else if (written != sizeof(buffer))
+	{
+		fprintf(stderr, "\nfailed to sent full data stream");
+		return 1;
+	}
 
-	return "request sent";
+	return 0;
 
-}	
+}
 
 char* HttprecvFullSSL(SSL* ssl)
 {
@@ -264,7 +275,7 @@ char* HttprecvFullSSL(SSL* ssl)
 	if (!buffer) return NULL;
 
 	int bytes;
-	
+
 	while ((bytes = SSL_read(ssl, buffer, 4095)) > 0)
 	{
 		buffer[bytes] = '\0';
@@ -298,68 +309,68 @@ const char* HttpTextcode(int status)
 
 	switch (status)
 	{
-		case 100: return "Continue";
-		case 101: return "Switching Protocols";
-		case 102: return "Processing";
-		case 103: return "Early Hints";
-		case 200: return "OK";
-		case 201: return "Created";
-		case 202: return "Accepted";
-		case 203: return "Non-Authoritative Information";
-		case 204: return "No Content";
-		case 205: return "Reset Content";
-		case 206: return "Partial Content";
-		case 207: return "Multi-Status";
-		case 208: return "Already Reported";
-		case 226: return "IM Used";
-		case 300: return "Multiple Choices";
-		case 301: return "Moved Permanently";
-		case 302: return "Found";
-		case 303: return "See Other";
-		case 304: return "Not Modified";
-		case 307: return "Temporary Redirect";
-		case 308: return "Permanent Redirect";
-		case 400: return "Bad Request";
-		case 401: return "Unauthorized";
-		case 402: return "Payment Required";
-		case 403: return "Forbidden";
-		case 404: return "Not Found";
-		case 405: return "Method Not Allowed";
-		case 406: return "Not Acceptable";
-		case 407: return "Proxy Authentication Required";
-		case 408: return "Request Timeout";
-		case 409: return "Conflict";
-		case 410: return "Gone";
-		case 411: return "Length Required";
-		case 412: return "Precondition Failed";
-		case 413: return "Payload Too Large";
-		case 414: return "URI Too Long";
-		case 415: return "Unsupported Media Type";
-		case 416: return "Range Not Satisfiable";
-		case 417: return "Expectation Failed";
-		case 418: return "I'm a teapot";
-		case 421: return "Misdirected Request";
-		case 422: return "Unprocessable Entity";
-		case 423: return "Locked";
-		case 424: return "Failed Dependency";
-		case 425: return "Too Early";
-		case 426: return "Upgrade Required";
-		case 428: return "Precondition Required";
-		case 429: return "Too Many Requests";
-		case 431: return "Request Header Fields Too Large";
-		case 451: return "Unavailable For Legal Reasons";
-		case 500: return "Internal Server Error";
-		case 501: return "Not Implemented";
-		case 502: return "Bad Gateway";
-		case 503: return "Service Unavailable";
-		case 504: return "Gateway Timeout";
-		case 505: return "HTTP Version Not Supported";
-		case 506: return "Variant Also Negotiates";
-		case 507: return "Insufficient Storage";
-		case 508: return "Loop Detected";
-		case 510: return "Not Extended";
-		case 511: return "Network Authentication Required";
-		default:  return "Unknown Status";
+	case 100: return "Continue";
+	case 101: return "Switching Protocols";
+	case 102: return "Processing";
+	case 103: return "Early Hints";
+	case 200: return "OK";
+	case 201: return "Created";
+	case 202: return "Accepted";
+	case 203: return "Non-Authoritative Information";
+	case 204: return "No Content";
+	case 205: return "Reset Content";
+	case 206: return "Partial Content";
+	case 207: return "Multi-Status";
+	case 208: return "Already Reported";
+	case 226: return "IM Used";
+	case 300: return "Multiple Choices";
+	case 301: return "Moved Permanently";
+	case 302: return "Found";
+	case 303: return "See Other";
+	case 304: return "Not Modified";
+	case 307: return "Temporary Redirect";
+	case 308: return "Permanent Redirect";
+	case 400: return "Bad Request";
+	case 401: return "Unauthorized";
+	case 402: return "Payment Required";
+	case 403: return "Forbidden";
+	case 404: return "Not Found";
+	case 405: return "Method Not Allowed";
+	case 406: return "Not Acceptable";
+	case 407: return "Proxy Authentication Required";
+	case 408: return "Request Timeout";
+	case 409: return "Conflict";
+	case 410: return "Gone";
+	case 411: return "Length Required";
+	case 412: return "Precondition Failed";
+	case 413: return "Payload Too Large";
+	case 414: return "URI Too Long";
+	case 415: return "Unsupported Media Type";
+	case 416: return "Range Not Satisfiable";
+	case 417: return "Expectation Failed";
+	case 418: return "I'm a teapot";
+	case 421: return "Misdirected Request";
+	case 422: return "Unprocessable Entity";
+	case 423: return "Locked";
+	case 424: return "Failed Dependency";
+	case 425: return "Too Early";
+	case 426: return "Upgrade Required";
+	case 428: return "Precondition Required";
+	case 429: return "Too Many Requests";
+	case 431: return "Request Header Fields Too Large";
+	case 451: return "Unavailable For Legal Reasons";
+	case 500: return "Internal Server Error";
+	case 501: return "Not Implemented";
+	case 502: return "Bad Gateway";
+	case 503: return "Service Unavailable";
+	case 504: return "Gateway Timeout";
+	case 505: return "HTTP Version Not Supported";
+	case 506: return "Variant Also Negotiates";
+	case 507: return "Insufficient Storage";
+	case 508: return "Loop Detected";
+	case 510: return "Not Extended";
+	case 511: return "Network Authentication Required";
+	default:  return "Unknown Status";
 	}
 
 
