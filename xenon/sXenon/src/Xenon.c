ed46acb89333523c9c3e8d023b785dab1ae330a5
koram kram <zorojoru31@gmail.com>
2026-06-04 00:29:58 +0400

    fixed ld error

--- xenon/sXenon/src/Xenon.c
+++ xenon/sXenon/src/Xenon.c
@@ -2,6 +2,7 @@
 
 addrctx CTX;
 struct sockaddr* sockinfo;
+int WSAres = -1;
 
 int main(int argc, char* arg[10])
 {
@@ -20,73 +21,84 @@ int main(int argc, char* arg[10])
 
         for (int i = 1; i < argc; i++)
         {
-            if (strcmp(arg[i], "-h") == 0 || strcmp(arg[i], "-help") == 0 || strcmp(arg[i], "?") == 0)
-            {
-				help_menu();
-			}
-            else if(strcmp(arg[i], "-v") == 0)
+
+            if (strcmp(arg[i], "-h") == 0 ||
+                strcmp(arg[i], "-help") == 0 ||
+                strcmp(arg[i], "?") == 0) {help_menu();}
+
+            else if (strcmp(arg[i], "-v") == 0)
             {
                 printf(WHITE"\n%s"BLACK, version());
             }
-            else if(strcmp(arg[i], "-init") == 0)
+
+            else if (strcmp(arg[i], "-init") == 0)
             {
-                int WSAres = -1;
-				WSAres = WININIT(WSAres);
+                WSAres = WININIT(WSAres);
             }
-            else if (strcmp(arg[i], "-conn")==0 || strcmp(arg[i], "-randconn") == 0)
+
+            else if (strcmp(arg[i], "-conn") == 0 || strcmp(arg[i], "-randconn") == 0)
             {
 
-                int port;
-                char* type = NULL;
-                char* family = NULL;
+                int port = -1;
+                char type[16] = { 0 };
+                char family[16] = { 0 };
 
-				for (int j = 0; j < argc; j++)
+                for (int j = 1; j < argc; j++)
                 {
-                    if (sscanf(arg[j], "-p %d", &port) == 1)
+                    if (strncmp(arg[j], "-p", 2) == 0)
                     {
+						port = atoi(arg[j] + 2);
                         xenon_init(&CTX, ANYADDR, port);
-						for (int k = 0; k < argc; k++)
-                        {
-                            if (sscanf(arg[k], "-%s", type) == 1)
-                            {
-                                type = arg[k];
-                                for (int l = 0; l < argc; l++)
-                                {
-                                    if (sscanf(arg[l], "-%5s", family) == 1)
-                                    {
+                    }
 
-										SOCKET sock = xenon_socket(type, family);
-                                        if(sock == INVALID_SOCKET)
-                                        {
-                                            fprintf(stderr, "\n[-] Failed to establish connection.\n");
-                                            return -1;
-										}
+                    if (strcmp(arg[j], "-TCP") == 0 || strcmp(arg[j], "-UDP") == 0 || strcmp(arg[j], "-TLS") == 0)
+                    {
+						type = arg[j];
+                    }
 
-										xenon_BL(sock, sockinfo);
-                                        __pexec(sock);
-
-                                        return 0;
-
-                                    }
-                                }
-                            }
-                        }
+                    if (strncmp(arg[j], "-ipv", 4) == 0)
+                    {
+						family = arg[j];
                     }
                 }
+
+                if (port == -1 || type[0] == NULL || family[0] == NULL)
+                {
+                    fprintf(stderr, "\nmissing arguments for -conn\n");
+                    return -1;
+                }
+
+                SOCKET sock = xenon_socket(type, family);
+
+                if (sock == INVALID_SOCKET)
+                {
+                    fprintf(stderr, "\nfailed to create socket\n");
+                    return -1;
+                }
+
+                xenon_BL(sock, sockinfo);
+                __pexec(sock);
+
+                return 0;
             }
+
             else if (strcmp(arg[i], "-sconn") == 0 || strcmp(arg[i], "-specconn") == 0)
             {
                 return 0;
             }
+
             else if (strcmp(arg[i], "-mconn") == 0 || strcmp(arg[i], "-multiconn") == 0)
             {
-				return 0;
+                return 0;
             }
-            else{
-				fprintf(stderr, "\ninvalid argument: %s", arg[i]);
-                return -1;
+
+            else
+            {
+                fprintf(stderr, "\ninvalid argument: %s", arg[i]);
+				return -1;
             }
         }
+
     }   
 
     return 0;
