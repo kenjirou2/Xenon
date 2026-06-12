1738fbf372a6ee00147968fb635765277f2a9715
koram kram <zorojoru31@gmail.com>
2026-06-12 17:38:37 +0400

    corrected mistakes in manual/ -help menu

--- xenon/sXenon/src/misc/menu.c
+++ xenon/sXenon/src/misc/menu.c
@@ -33,13 +33,13 @@ void help_menu(void)
     printf("\t\n -v : Version");
     printf("\t\n -v? : Http status code");
     printf("\t\n -h/ -help / ? : Help");
-    printf("\t\n -s : select [arguments]"BLACK);
+    printf("\t\n -s : select [arguments]");
     printf("\t\n -conn : [port]");
-    printf("\t\n         Accept connection from a random node");
+    printf("\t\n         Accept connection from a random node\n");
     printf("\t\n -sconn : [family] [type] [port] [address]");
     printf("\t\n          Accept connection from specific node\n");
     printf("\t\n -mconn : [port]");
-    printf("\t\n          Get a list of all nodes connected to your device you can use (-select [ID]) to accept connection")
+    printf("\t\n          Get a list of all nodes connected to your device you can use (-select [ID]) to accept connection"BLACK);
     return;
 }
 
