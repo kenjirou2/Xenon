b89e6120a3de69cfadc12a605f1f65de2866fb67
kenjirou2 <zorojor31@gmail.com>
2026-06-19 15:04:00 +0000

    working on Xenon for linux and making headers and linkables compatible

--- xenon/sXenon/Dependencies/Xenon/xenon.h
+++ xenon/sXenon/Dependencies/Xenon/xenon.h
@@ -10,6 +10,7 @@
 #include "../../../Include/ansiiC/AnsiiC.h"
 
 
+
 typedef struct
 {
 
@@ -19,13 +20,20 @@ typedef struct
 } addrctx, *paddrctx;
 
 
+
+
 #if defined(_WIN32) || defined(_WIN64)
 	
 	#include "../../../Include/driver/socket.h"
 	#include "../../../Include/driver/windows.h"
 
 int WININIT(int WSAres);
-        
+
+SOCKET xenon_socket(const char* type, const char* family);
+int xenon_BL(SOCKET Socket);
+int xenon_bl_ex(SOCKET Socket, addrctx* CTX);
+
+
 #elif defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__) || defined(__APPLE__)
 
 	#include <sys/socket.h>
@@ -33,15 +41,23 @@ int WININIT(int WSAres);
 	#include <arpa/inet.h>
 	#include <unistd.h>
 
+
+#define INVALID_SOCKET (-1)
+#define SOCKET_ERROR (-1)
+
+
+    int xenon_socket(const char* type, const char* family);
+    int xenon_BL(int Socket);
+    int xenon_bl_ex(int Socket, addrctx* CTX);
+
+
 #else
 
 #error "\a\nUnsupported Operating system"
 
+
 #endif
 
 addrctx* xenon_init(addrctx* CTX, char* addr, int port);
-SOCKET xenon_socket(const char* type, const char* family);
-int xenon_BL(SOCKET Socket);
-int xenon_bl_ex(SOCKET Socket, addrctx* CTX);
 
 #endif
