d503d79283c6cb6186731057ec904b205cc74f15
kenjirou2 <zorojor31@gmail.com>
2026-06-20 16:10:51 +0000

    fixed some Include/ files

--- xenon/Include/IP/ip.h
+++ xenon/Include/IP/ip.h
@@ -1,30 +1,6 @@
 #ifndef IP_H
 #define IP_H
 
-int ipint(char* IP)
-{
-    int ip = 0;
-    int num = 0;
-
-    while (*IP)
-    {
-        if (*IP >= '0' && *IP <= '9')
-        {
-            num = num * 10 + (*IP - '0');
-        }
-        else if (*IP == '.')
-        {
-            ip = (ip << 8) | num;
-            num = 0;
-        }
-        IP++;
-    }
-
-    ip = (ip << 8) | num;
-
-    return ip;
-}
-
 int ipcmp(const char* ip)
 {
     int a, b, c, d;
