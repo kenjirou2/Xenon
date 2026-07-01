57fb5a8b51a9ce1d0c5f04d111af8e9cfd86090b
kenjirou2 <zorojoru31@gmail.com>
2026-07-01 16:51:54 +0400

    DELL everything will work later (#3--)

--- xenon/Include/driver/windows.h
+++ xenon/Include/driver/windows.h
@@ -5,17 +5,6 @@
 
 #include <windows.h>
 
-typedef struct
-{
-
-	IMAGE_DOS_HEADER;
-	IMAGE_NT_HEADERS;
-	IMAGE_OPTIONAL_HEADER;
-	IMAGE_FILE_HEADER;
-	IMAGE_SECTION_HEADER;
-
-} PE_HEADER;
-
 #else
 
 #error "windows.h cannot be used in this kernel."
